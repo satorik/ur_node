516d5120823d43bd6b4012f709b9249a03a6a10d
satorik <satorik@yandex.ru>
2020-04-21 00:02:25 +0300

    set env vars

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import 'regenerator-runtime/runtime'
 import server from './server'
 import populate from './utils/populate'
 
+require('dotenv').config()
+
 populate(false).
   then(res => {
     server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
